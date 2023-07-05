2741fa05db32f9fd65d3db35bc336f4abfd4e881
handika al falaq <122656201+Handikaalfalaq@users.noreply.github.com>
2023-07-05 13:57:28 +0700

    Add files via upload

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -1,144 +1,138 @@
-import { UserContext } from "../context/UserContext";
-import React, { useContext } from 'react';
+import {UserContext} from "../context/UserContext";
+import {React,  useContext} from 'react';
 import './assets/index.css';
 import { Container } from 'react-bootstrap';
 import FolderImage from './assets/img/folderImg';
-import { useQuery, useMutation } from 'react-query';
-import { API } from '../config/Api';
-import { useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2';
+import {useQuery, useMutation} from 'react-query';
+import {API} from '../config/Api';
+import { useNavigate} from 'react-router-dom';
+import Swal from 'sweetalert2'
 
 function Home() {
-  const navigate = useNavigate();
-  const [state] = useContext(UserContext);
+    const navigate = useNavigate()
+    const [state] = useContext(UserContext)
+    
+    const { data: transactionBook } = useQuery("transactionBookCache", async () => {
+        const responseTransaction = await API.get(`/transaction/${state.user.id}`);
+        const dataBookById = responseTransaction.data.data;
+        console.log("transactionBook", transactionBook);
+        console.log("transactionBook1", dataBookById);
+        console.log("transactionBook2", dataBookById[0].transactionBooks[0].book.id);
+      
+        // Mencetak semua book.id
+        dataBookById.forEach((transaction) => {
+          transaction.transactionBooks.forEach((book) => {
+            console.log("book.id:", book.book.id);
+          });
+        });
+      });
+      
 
-  const { data: transactionBook } = useQuery("transactionBookCache", async () => {
-    const responseTransaction = await API.get(`/transaction/${state.user.id}`);
-    const dataBookById = responseTransaction.data.data;
-    return dataBookById;
-  });
+    const {data: dataAllBook, isLoading: isLoadingAllBook}= useQuery("dataAllTripCache", async () => {
+    const response = await API.get("/books")
+    return response.data.data
+    })
 
-  const { data: dataAllBook, isLoading: isLoadingAllBook } = useQuery("dataAllTripCache", async () => {
-    const response = await API.get("/books");
-    return response.data.data;
-  });
+    let dataSorted = [];
+    let topTwoData = [];
 
-  let dataSorted = [];
-  let topTwoData = [];
-
-  if (!isLoadingAllBook) {
+    if (!isLoadingAllBook) {
     dataSorted = [...dataAllBook].sort((a, b) => b.sold - a.sold);
     topTwoData = dataSorted.slice(0, 2);
-  }
-
-  const handleSubmit = useMutation(async (id) => {
-    try {
-      if (!state.isLogin) {
-        return Swal.fire({
-          icon: 'warning',
-          title: 'Silahkan login terlebih dahulu',
-        });
-      }
-
-      const config = {
-        headers: {
-          'Content-type': 'multipart/form-data',
-        },
-      };
-
-      const formData = new FormData();
-      formData.set('idBook', id);
-      formData.set('idUser', state.user.id);
-      const response = await API.post('/cart', formData, config);
-      console.log("add book success : ", response);
-    } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Buku sudah ada di keranjang',
-      });
-      console.log("add book failed : ", error);
     }
-  });
 
-  return (
+    const handleSubmit = useMutation(async (id) => {
+        try {
+            // e.preventDefault();
+            if ( !state.isLogin ) {
+                return Swal.fire({
+                    icon: 'warning',
+                    title: 'Silahkan login terlebih dahulu',
+                    })
+            }
+
+            const config = {
+                headers: {
+                    'Content-type': 'multipart/form-data',
+                },
+                };
+
+            const formData = new FormData();
+            formData.set('idBook', id);
+            formData.set('idUser', state.user.id);
+            const response = await API.post('/cart', formData, config);
+            console.log("add book success : ", response);
+        } catch (error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Buku sudah ada di keranjang',
+                })
+            console.log("add book failed : ", error)
+        } 
+    });
+
+return (
     <Container className='containerHome'>
-      <h1 className='titleHome'>
-        With us, you can shop online & help save your high street at the same time
-      </h1>
+        <h1 className='titleHome'> 
+            With us, you can shop online & help save your high street at the same time
+        </h1>
 
-      <div className='containerBookTop'>
-        {topTwoData.map((item, index) => {
-          return (
-            <div key={index} className='bookTop'>
-              <div className='imageBestSeller' style={{ backgroundImage: `url(${FolderImage.BestSeller})` }}></div>
-              <div className='imageBook' style={{ backgroundImage: `url(${item.image})` }}></div>
-              <div className='textBook'>
-                <div className='title' onClick={(e) => { navigate(`/DetailBook/${index}`) }}>{item.title}</div>
-                <div className='author'>By. {item.author}</div>
-                <div className='closing'>{item.description}</div>
-                <div className='closing'>{item.id}</div>
+        <div className='containerBookTop'>
+            {topTwoData.map((item, index) => {
+                return (
+                    <div key={index} className='bookTop'>
+                        <div className='imageBestSeller' style={{backgroundImage: `url(${FolderImage.BestSeller})`}}></div>
+                        <div className='imageBook' style={{backgroundImage: `url(${item.image})`}}></div>
+                        <div className='textBook'>
+                            <div className='title' onClick={(e) => {navigate(`/DetailBook/${index}`)}}>{item.title}</div>
+                            <div className='author'>By. {item.author}</div>
+                            <div className='closing'>{item.description}</div>
+                            <div className='closing'>{item.id}</div>
 
-                {item.discount !== 0 ? (
-                  <div style={{ display: 'flex', marginBottom: '10px' }}>
-                    <div className='priceListBookLine'>Rp. {item.price.toLocaleString()}</div>
-                    <div className='priceListBook'>Rp. {item.priceDiscount.toLocaleString()}</div>
-                  </div>
-                ) : (
-                  <div className='priceListBook'>Rp. {item.price.toLocaleString()}</div>
-                )}
+                            {item.discount !== 0 ? (
+                                <div style={{display: 'flex', marginBottom: '10px'}}>
+                                    <div className='priceListBookLine'>Rp. {item.price.toLocaleString()}</div>
+                                    <div className='priceListBook'>Rp. {item.priceDiscount.toLocaleString()}</div>
+                                </div>
+                            ) : (
+                                <div className='priceListBook'>Rp. {item.price.toLocaleString()}</div>
+                            )}
+                            
 
-                {transactionBook.map((transaction, index) => {
-                  return (
-                    <div key={index}>
-                      {transaction.transactionBooks.filter((book, bookIndex) => {
-                        if (book.book.id === item.id) {
-                          return (
-                            <div className='downloadMyBook' key={bookIndex}>
-                              Download
-                            </div>
-                          );
-                        } else {
-                          return (
-                            <button className='buttonCart' key={bookIndex} onClick={() => handleSubmit.mutate(item.id)}>
-                              Add to Cart
-                            </button>
-                          );
-                        }
-                      })}
+                            <button className='buttonCart' onClick={() => handleSubmit.mutate(item.id)}>Add to Cart</button>
+
+                        </div>
                     </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
-      </div>
+                )
+            })}
+             
+        </div>
 
-      <div className='textTitleListBook'>List Book</div>
-      <div className='containerListBook'>
-        {dataAllBook?.map((item, index) => {
-          return (
-            <div key={index} className='listBook'>
-              <div className='imageListBook' style={{ backgroundImage: `url(${item.image})` }}></div>
-              <div className='titleListBook' onClick={(e) => { navigate(`/DetailBook/${index}`) }}>{item.title}</div>
-              <div className='authorListBook'>{item.author}</div>
+        <div className='textTitleListBook'>List Book</div>
+        <div className='containerListBook'>
+            {dataAllBook?.map((item, index) => {
+                return(
+                    <div key={index} className='listBook'>
+                        <div className='imageListBook' style={{backgroundImage: `url(${item.image})`}}></div>
+                        <div className='titleListBook' onClick={(e) => {navigate(`/DetailBook/${index}`)}}>{item.title}</div>
+                        <div className='authorListBook'>{item.author}</div>
 
-              {item.discount !== 0 ? (
-                <div style={{ display: 'flex', marginBottom: '10px' }}>
-                  <div className='priceListBookLine'>Rp. {item.price.toLocaleString()}</div>
-                  <div className='priceListBook'>Rp. {item.priceDiscount.toLocaleString()}</div>
-                </div>
-              ) : (
-                <div className='priceListBook'>Rp. {item.price.toLocaleString()}</div>
-              )}
+                        {item.discount !== 0 ? (
+                            <div style={{display: 'flex', marginBottom: '10px'}}>
+                                <div className='priceListBookLine'>Rp. {item.price.toLocaleString()}</div>
+                                <div className='priceListBook'>Rp. {item.priceDiscount.toLocaleString()}</div>
+                            </div>
+                        ) : (
+                            <div className='priceListBook'>Rp. {item.price.toLocaleString()}</div>
+                        )}
 
-            </div>
-          );
-        })}
-      </div>
+                    </div>
+                )
+            })}
+        </div>
 
     </Container>
-  );
+);  
 }
 
-export default Home;
+export default Home
